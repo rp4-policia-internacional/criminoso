f4e43084b521a7434f5f36212bebfde5ffc6d1b9
sabrina <sabrinarfernandes.srf@gmail.com>
2023-09-18 18:21:50 -0300

    medança no getOne

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,14 @@
 import CriminosoRepository from "@modules/criminoso/infra/prisma/repositories/CriminosoRepository";
 import ICriminosoRepository from "@modules/criminoso/repository/ICriminosoRepository";
+import OrganizacaoRepository from "@modules/organizacao/infra/prisma/repositories/OrganizacaoRepository";
+import IOrganizacaoRepository from "@modules/organizacao/repository/IOrganizacaoRepository";
 import { container } from "tsyringe";
 
 container.registerSingleton<ICriminosoRepository>(
     'CriminosoRepository',
     CriminosoRepository
+);
+container.registerSingleton<IOrganizacaoRepository>(
+    'OrganizacaoRepository',
+    OrganizacaoRepository
 );
